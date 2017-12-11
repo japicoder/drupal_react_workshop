c0b40bf66d901a5aff6366e7d995cff63d614cc1
Javier Díaz Jiménez <japicoder@gmail.com>
2017-12-11 11:47:24 +0100

    Added drw module

--- drw/web/sites/default/settings.php
+++ drw/web/sites/default/settings.php
@@ -295,7 +295,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'OuV_Cmc5kcmcoivEERyybVVbyR4HiVeoJR4YrPnAocTEaTK3KXNxNQNuPFFGp9HAPaobi5r5vw';
 
 /**
  * Deployment identifier.
@@ -784,3 +784,14 @@ $settings['entity_update_batch_size'] = 50;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = array (
+  'database' => 'drw',
+  'username' => 'drw',
+  'password' => 'drwtestpass',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
